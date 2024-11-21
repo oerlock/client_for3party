f10c011e51426a91d4d6af97924bfc6ec73beaed
erlock <oerlock@gmail.com>
2024-11-21 15:05:40 +0800

    alter the way about trace[2]

--- tests/client_for3party/test_client.py
+++ tests/client_for3party/test_client.py
@@ -6,6 +6,6 @@ from src.client_for3party.client import ClientBase
 @pytest.mark.asyncio
 async def test_client_base():
     async with ClientBase('https://google.com') as client:
-        async with client.session.get(f'{client.base_url}/') as resp:
+        async with client.session.get('/') as resp:
             data = await resp.text()
     assert data
